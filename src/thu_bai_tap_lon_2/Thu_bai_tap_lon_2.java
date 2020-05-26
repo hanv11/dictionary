3bca5e0889e3d9f5e214b696d33214a35a34d8fe
Nguyen Van Ha <hanguyen11mm@gmail.com>
2020-05-26 14:53:16 +0700

    init project

---
+++ src/thu_bai_tap_lon_2/Thu_bai_tap_lon_2.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package thu_bai_tap_lon_2;
+
+/**
+ *
+ * @author tuan
+ */
+public class Thu_bai_tap_lon_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MainWindown mw = new MainWindown();
+        mw.show();
+    }
+    
+}
